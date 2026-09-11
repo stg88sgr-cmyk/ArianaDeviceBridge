e7e2b88d2b405f46fe24ce20f5e53e7fee1e5d2a
stg88sgr-cmyk <stg.88.sgr@gmail.com>
2026-09-11 18:16:59 +0200

    Bump Presence Lite to v2.1 stable-signing build

--- android/presence-lite/app/build.gradle.kts
+++ android/presence-lite/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.snowworks.presencelite"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.1"
     }
 
     compileOptions {
