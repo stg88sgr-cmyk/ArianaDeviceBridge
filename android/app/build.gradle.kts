20dc376af398d2661619e50916aa9535e35f9876
stg88sgr-cmyk <stg.88.sgr@gmail.com>
2026-09-12 12:21:41 -0700

    X-88 local memory: bump version 1.4.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "de.snowworks.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.4.2"
+        versionCode = 8
+        versionName = "1.4.3"
     }
 
     signingConfigs {
