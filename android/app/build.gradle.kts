3b4d09b432844c6ce5ad2cab5dd117537720164d
stg88sgr-cmyk <stg.88.sgr@gmail.com>
2026-09-13 12:53:40 +0200

    build: bump sideload fix to 1.4.7

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "de.snowworks.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.4.6"
+        versionCode = 12
+        versionName = "1.4.7"
     }
 
     signingConfigs {
