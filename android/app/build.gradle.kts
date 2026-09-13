f677c5549297cd9a5ab75218808e35b4dc03430f
stg88sgr-cmyk <stg.88.sgr@gmail.com>
2026-09-13 15:20:35 +0200

    release: bump Ariana X-88 to 1.4.10

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "de.snowworks.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.4.9"
+        versionCode = 15
+        versionName = "1.4.10"
     }
 
     signingConfigs {
