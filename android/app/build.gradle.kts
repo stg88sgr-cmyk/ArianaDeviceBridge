6c54bad5bee89a53820ab562fe8b29d282ad95d8
stg88sgr-cmyk <310410865+stg88sgr-cmyk@users.noreply.github.com>
2026-09-15 16:38:41 +0200

    Cut Presence v2 RC1 code 37

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "de.snowworks.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 36
-        versionName = "1.13.0-rc1"
+        versionCode = 37
+        versionName = "1.14.0-rc1"
     }
 
     signingConfigs {
