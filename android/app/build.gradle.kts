7a3e9d45de29be2001cce7cbbb7be5bd04006b1d
stg88sgr-cmyk <stg.88.sgr@gmail.com>
2026-09-16 11:02:45 +0200

    Bump bootstrap to code 43 for Muse reasoning budget

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "de.snowworks.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 42
-        versionName = "1.14.2-rc1"
+        versionCode = 43
+        versionName = "1.14.3-rc1"
     }
 
     signingConfigs {
