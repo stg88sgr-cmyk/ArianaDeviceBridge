94f9edc1257458f6655e265320ade30a298ee90c
stg88sgr-cmyk <stg.88.sgr@gmail.com>
2026-09-16 11:22:24 +0200

    Bump bootstrap to code 44 for stable core health

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "de.snowworks.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 43
-        versionName = "1.14.3-rc1"
+        versionCode = 44
+        versionName = "1.14.4-rc1"
     }
 
     signingConfigs {
