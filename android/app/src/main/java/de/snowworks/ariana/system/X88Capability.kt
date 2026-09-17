a60735c9f37861435af236e629b912692a05eeff
stg88sgr-cmyk <stg.88.sgr@gmail.com>
2026-09-17 02:58:04 +0200

    feat(x88): add canonical system capabilities

---
+++ android/app/src/main/java/de/snowworks/ariana/system/X88Capability.kt
@@ -0,0 +1,16 @@
+package de.snowworks.ariana.system
+
+enum class X88Capability {
+    CAMERA,
+    MICROPHONE,
+    SCREEN_CAPTURE,
+    FILES,
+    NOTIFICATIONS,
+    ACCESSIBILITY,
+    LOCATION,
+    BLUETOOTH,
+    APP_CONTROL,
+    MEDIA_CONTROL,
+    AI_ROUTING,
+    UPDATE_CONTROL,
+}
