b275a3cfcef8068665b9f591e8c889b56877ef36
stg88sgr-cmyk <stg.88.sgr@gmail.com>
2026-09-17 10:39:44 +0200

    build(x88): version Android package for V31

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "de.snowworks.app"
         minSdk = 28
         targetSdk = 36
-        versionCode = 47
-        versionName = "1.14.7-rc1"
+        versionCode = 48
+        versionName = "1.15.0-v31"
     }
 
     signingConfigs {
