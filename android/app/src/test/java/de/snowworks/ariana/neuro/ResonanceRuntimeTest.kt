2b403dc96cb4983e4ffd5706238431f51745a013
stg88sgr-cmyk <stg.88.sgr@gmail.com>
2026-09-21 09:44:53 +0200

    Fix resonance test JUnit imports

--- android/app/src/test/java/de/snowworks/ariana/neuro/ResonanceRuntimeTest.kt
+++ android/app/src/test/java/de/snowworks/ariana/neuro/ResonanceRuntimeTest.kt
@@ -1,7 +1,8 @@
 package de.snowworks.ariana.neuro
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Test
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 
 class ResonanceRuntimeTest {
     @Test
